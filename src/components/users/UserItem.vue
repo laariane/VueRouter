1d62641cc75a98bbc72ddf232515bfea8e3fe050
laariane <laarianeamine@gmail.com>
2023-06-10 14:23:31 +0200

    team member application case for testing

---
+++ src/components/users/UserItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <li>
+    <h3>{{ name }}</h3>
+    <div class="role" :class="roleClass">{{ role }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['name', 'role'],
+  computed: {
+    roleClass() {
+      switch (this.role) {
+        case 'Engineer':
+          return 'role--engineer';
+        case 'Consultant':
+          return 'role--consultant';
+        case 'Student':
+          return 'role--student';
+        default:
+          return null;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+h3 {
+  margin: 0.5rem 0;
+}
+
+.role {
+  border-radius: 40px;
+  background-color: #ccc;
+  color: #252525;
+  padding: 0.25rem 1rem;
+  display: inline-block;
+}
+
+.role--engineer {
+  background-color: blue;
+  color: white;
+}
+
+.role--consultant {
+  background-color: #af003a;
+  color: white;
+}
+.role--student {
+  background-color: #cdce83;
+  color: white;
+}
+</style>
---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <button @click="confirmInput()">confirm</button>
+
+  </ul>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  methods:{
+    confirmInput(){
+      this.$router.push('/teams')
+
+    }
+
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
